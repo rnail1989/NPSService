69cf773371c6cbf177e65097e117336c6a757e26
rnail1989 <rnail1989@mail.ru>
2020-10-29 19:45:57 +0300

    Update metods

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import tech.itpark.NPSService.NPSService;
+
+public class Main {
+    public static void main(String[] args) {
+        new NPSService();
+    }
+}
---
+++ src/tech/itpark/NPSService/NPSService.java
@@ -0,0 +1,16 @@
+package tech.itpark.NPSService;
+
+public class NPSService {
+    public void NPS(int [] votes){
+
+    int promoters=0;
+    int neutral=0;
+    int enemy=0;
+    int count=0;
+
+
+
+
+    }
+
+}
