6da9f200c2555e8f721dde99318ea0201dd63445
rnail1989 <rnail1989@mail.ru>
2020-11-06 12:03:57 +0300

    fixed #1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import tech.itpark.NPSService.NPSService;
+import tech.itpark.npsService.NPSService;
 
 public class Main {
     public static void main(String[] args) {
         NPSService service = new NPSService();
 
-        System.out.println(service.nps(new int[]{2, 3, 10,9,5,7,7,5,3,1,10,10}));
+        System.out.println(service.nps(new int[]{2, 3, 10, 9, 5, 7, 7, 5, 3, 1, 10, 10}));
     }
 
 }
--- src/tech/itpark/npsService/NPSService.java
+++ src/tech/itpark/npsService/NPSService.java
@@ -1,29 +1,25 @@
 package tech.itpark.npsService;
 
 public class NPSService {
-    public double nps(int [] votes){
+    public double nps(int[] votes) {
 
-    int promoters=0;
-    int enemy=0;
-    int count=0;
+        int promoters = 0;
+        int enemy = 0;
+        int count = 0;
 
         for (int i = 0; i < votes.length; i++) {
-            if (votes[i]>8 && votes[i]<=10) {
+            if (votes[i] > 8 && votes[i] <= 10) {
                 promoters++;
             }
-            if (votes[i]>=0 && votes[i] < 7 ) {
+            if (votes[i] >= 0 && votes[i] < 7) {
                 enemy++;
             }
-            if(votes[i]>=0 && votes[i]<=10){
+            if (votes[i] >= 0 && votes[i] <= 10) {
                 count++;
             }
         }
         double nps = promoters * 100.0 / count - enemy * 100.0 / count;
         return nps;
-
-
-
-
 
 
     }
